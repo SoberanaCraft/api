c5144e9ae4db156ab1983d9213d8142ba7ee189c
roridev <roridev@protonmail.com>
2023-03-26 14:44:43 -0300

    Add AuthenticatedUser.kt

---
+++ src/main/kotlin/net/soberanacraft/api/models/AuthenticatedUser.kt
@@ -0,0 +1,24 @@
+package net.soberanacraft.api.models
+
+import kotlinx.serialization.Serializable
+import net.soberanacraft.api.UUIDSerializer
+import org.jetbrains.exposed.sql.ResultRow
+import org.jetbrains.exposed.sql.Table
+import java.util.UUID
+
+@Serializable
+data class AuthenticatedUser(@Serializable(with=UUIDSerializer::class) val owner: UUID, val discordId: ULong, val password: String)
+
+object Authentication: Table () {
+    val owner = uuid("owner")
+    val discordId = ulong("discordId")
+    val password = text("password")
+
+    override val primaryKey = PrimaryKey(owner)
+}
+
+fun ResultRow.intoAuthenticatedUser() = AuthenticatedUser(
+    owner = this[Authentication.owner],
+    discordId = this[Authentication.discordId],
+    password = this[Authentication.password]
+)
